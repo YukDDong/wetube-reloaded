a3dc6d598bd2273b4be5609dba0c58d9c45615d7
YukDDong <ekfhd5537@naver.com>
2023-02-05 00:09:05 +0900

    2/4 study_ middleware, req, res

--- src/server.js
+++ src/server.js
@@ -4,9 +4,37 @@ const PORT = 4000;
 
 const app = exrpess();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if(url === "/protected"){
+    return res.send("<h1>Not Allowed</h1>");
+  };
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("<h1>I still love you.</h1>")
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login here.")
+}
+
+const handleProtected = (req, res) => {
+  return res.send("Protected!")
+}
+
+app.use(logger); 
+// app.use 는 모든 route에서 middleware를 사용하게 해준다.
+app.use(privateMiddleware);
 app.get("/", handleHome);
+app.get("/login", handleLogin);
+app.get("/protected", handleProtected)
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);

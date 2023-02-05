8b7d69d47fedfbc24d88daab9d85837636a96906
YukDDong <ekfhd5537@naver.com>
2023-02-05 16:06:24 +0900

    2/5 study_router,module,morgan(middleware)

--- src/server.js
+++ src/server.js
@@ -1,40 +1,21 @@
-import exrpess from "express";
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videosRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
-const app = exrpess();
+const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+app.use(logger);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if(url === "/protected"){
-    return res.send("<h1>Not Allowed</h1>");
-  };
-  next();
-};
 
-const handleHome = (req, res) => {
-  return res.send("<h1>I still love you.</h1>")
-};
 
-const handleLogin = (req, res) => {
-  return res.send("Login here.")
-}
-
-const handleProtected = (req, res) => {
-  return res.send("Protected!")
-}
-
-app.use(logger); 
-// app.use 는 모든 route에서 middleware를 사용하게 해준다.
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected)
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
